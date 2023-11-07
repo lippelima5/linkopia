61aac42bf81cb76a697832a1296ceb4952a1e2a7
lippelima5 <oliveiralippe@gmail.com>
2023-11-06 21:37:16 -0300

    Publish Linkopia

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1>Linkopia</h1>
+
+![Markware](.github/linkopia.png)
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+php -S 0.0.0.0:8000 
+```
+
+Open [http://localhost:8000](http://localhost:8000) with your browser to see the result.
---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <meta name="description" content="Criamos soluções personalizadas de BI, Dashboards, Analise de dados, Definição de Health-KPIs, DataFlow, Notificações Inteligentes, Data Warehouse, Engenharia de Dados e Machine Learning para otimizar sua empresa e impulsionar seu sucesso.">
+    <meta name="keywords" content="soluções personalizadas, BI, Engenharia de Dados, Machine Learning, otimização de empresa, sucesso, Dashboards, Analise de dados, Definição de Health-KPIs, DataFlow, Notificações Inteligentes, Data Warehouse">
+
+    <meta property="og:title" content="Markware Links">
+    <meta property="og:description" content="Criamos soluções personalizadas de BI, Engenharia de Dados e Machine Learning para otimizar sua empresa e impulsionar seu sucesso.">
+    <meta property="og:image" content="/LogoRound.webp">
+    <meta property="og:url" content="https://linkopia.xyz/">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Markware Links">
+    <meta name="twitter:description" content="Criamos soluções personalizadas de BI, Engenharia de Dados e Machine Learning para otimizar sua empresa e impulsionar seu sucesso.">
+    <meta name="twitter:image" content="/LogoRound.webp">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0P4BCCKJ8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-F0P4BCCKJ8');
+    </script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <title>Markware Links</title>
+    <style>
+        body {
+            margin-top: 75px;
+            padding: 0;
+            /* background-color: #f4f4f4; */
+          background-image: url('/bg.webp');        
+        }
+
+        .card {
+            border-radius: 30px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            background-color: #fff;
+            text-align: center;
+        }
+
+        .profile-image {
+            width: 150px;
+            height: 150px;
+            border: 5px solid #fff;
+            border-radius: 50%;
+            margin: -75px auto 10px;
+            position: relative;
+        }
+
+        .social-icons {
+            display: flex;
+            justify-content: space-evenly;
+        }
+
+        .social-icons a {
+            text-decoration: none;
+            color: #000;
+            margin: 10px;
+        }
+
+
+
+        a:hover {
+            transform: scale(1.04);
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container text-center">
+        <div class="row justify-content-center align-items-center" style="height: 100vh;">
+            <div class="col-xl-6">
+                <div class="card">
+                    <img class="profile-image rounded-circle" src="https://markware.com.br/img/LogoRound.svg" alt="Imagem de Perfil">
+                    <h4 class="mt-3">Markware</h4>
+                    <span class="text-muted">A Tecnologia ao alcance de todos.</span>
+                    <span class="text-muted">@markwareltda</span>
+                    <div class="mt-3">
+                        <div class="social-icons">
+                            <a href="/qr-vcard.png" target="_blank" title="QR Code">
+                                <i class="las la-qrcode fs-1"></i> <br>
+                                QR Code
+                            </a>
+                            <a class="btn btn-success rounded-5 text-white py-2 px-4 d-flex justify-content-center align-items-center" href="data:text/vcard;charset=utf-8,BEGIN:VCARD%0AVERSION:3.0%0AFN:Markware%0AORG:Markware%20Ltda%0ATEL:11917277726%0AEMAIL:contato@markware.com.br%0AURL:https://www.markware.com.br%0AX-WA-PhoneNumber:11917277726%0AEND:VCARD">Salvar
+                                na Agenda</a>
+                            <a href="#" onclick="share()">
+                                <i class="las la-share fs-1"></i> <br>
+                                Enviar Cartão
+                            </a>
+                        </div>
+                    </div>
+                    <div class="mt-5">
+                        <div class="social-icons">
+                            <a href="tel:+5511917277726">
+                                <i class="las la-mobile fs-1"></i> <br>
+                                Ligar
+                            </a>
+                            <a href="https://wa.me/5511917277726?text=Eu%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços">
+                                <i class="lab la-whatsapp fs-1"></i><br>
+                                Whatsapp
+                            </a>
+                            <a target="_blank" href="https://maps.app.goo.gl/gED8kBSaQWzb53hL9">
+                                <i class="las la-map-marked fs-1"></i><br>
+                                Mapa
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class=" mt-4">
+                        <h4></h4>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" target="_blank" href="https://wa.me/5511917277726?text=Eu%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços">
+                            <i class="lab la-whatsapp fs-1 me-3"></i> Clique aqui para chamar no Whatsapp
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" target="_blank" href="https://www.instagram.com/markwareltda/">
+                            <i class="lab la-instagram fs-1 me-3"></i> Instagram
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" href="tel:+5511917277726">
+                            <i class="las la-phone fs-1 me-3"></i> Telefone
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" target="_blank" href="https://www.facebook.com/markwareltda">
+                            <i class="lab la-facebook fs-1 me-3"></i> Facebook
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" target="_blank" href="https://markware.com.br/">
+                            <i class="las la-globe fs-1 me-3"></i> Site
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" href="mailto:contato@markware.com.br">
+                            <i class="las la-envelope fs-1 me-3"></i> E-mail
+                        </a>
+
+                        <a class="btn btn-dark mt-3 d-flex align-items-center" href="https://www.linkedin.com/company/markwareltda">
+                            <i class="lab la-linkedin fs-1 me-3"></i> LinkedIn
+                        </a>
+                    </div>
+
+                    <div class="my-4">
+
+                    </div>
+                </div>
+
+
+
+                <div class="mt-4">
+                    <span class="text-center ">© 2023 Markware. Todos os direitos reservados.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        function share() {
+            let url = document.location.href;
+            const canonicalElement = document.querySelector('link[rel=canonical]');
+            if (canonicalElement !== null) {
+                url = canonicalElement.href;
+            }
+
+            const shareOptions = {
+                title: 'Compartilhar este link',
+                text: 'Confira este link interessante:',
+                url: url,
+            };
+
+            if (navigator.share) {
+                // Se o navegador suportar a API Web Share, use-a
+                navigator.share(shareOptions)
+                    .then(() => {
+                        console.log('URL compartilhado com sucesso.');
+                    })
+                    .catch(error => {
+                        console.error('Erro ao compartilhar o URL: ', error);
+                    });
+            }
+        }
+    </script>
+</footer>
+
+</html>
